0229d2339b66a6a39ecf6fcedbbb45acb262d38e
Emilio Resende <emilio.resende@coxautoinc.com>
2021-03-13 10:53:01 -0300

    wip: forms done right (useRef)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import ImcTableView from './views/ImcTableView';
 import ImcView from './views/ImcView';
@@ -12,16 +12,15 @@ function App() {
   const [controller,] = useState(new ImcController());
   const [person, setPerson] = useState(new Person());
 
-  const calculateImc = async () => {
-    const heightElem = document.querySelector("#altura");
-    const weightElem = document.querySelector("#peso");
-  
-    if(!heightElem) throw Error("height is required field!");
-    if(!weightElem) throw Error("weight is required field!");
+  const heightElem = useRef();
+  const weightElem = useRef();
+
+  const calculateImc = async (evt) => {
+    evt.preventDefault();
 
     var newPerson = new Person(
-      parseFloat(heightElem.value), 
-      parseFloat(weightElem.value));
+      parseFloat(heightElem.current.value), 
+      parseFloat(weightElem.current.value));
 
     const personCalculated = await controller.calculate(newPerson.toObject());
     setPerson(personCalculated);
@@ -35,15 +34,17 @@ function App() {
             <ImcTableView />
           </div>
           <hr />
-          <div className="row">
-            <label>Altura</label>
-            <input id="altura" placeholder="0.00" />
-          </div>
-          <div className="row">
-            <label>Peso</label>
-            <input id="peso" placeholder="0.00" />
-          </div>
-          <button type="button" onClick={calculateImc} className="action">Calcular</button>
+          <form onSubmit={calculateImc}>
+            <div className="row">
+              <label>Altura</label>
+              <input id="altura" ref={heightElem} placeholder="0.00" />
+            </div>
+            <div className="row">
+              <label>Peso</label>
+              <input id="peso" ref={weightElem} placeholder="0.00" />
+            </div>
+            <button type="submit" className="action">Calcular</button>
+          </form>
         </div>
       </div>
       <hr />
